3c44aa1a301860b43a2e6e48b270e1174b60943b
maki-haji <moafarah99@gmail.com>
2023-10-15 23:06:33 +0100

    modified by maki

--- New-project.tf
+++ New-project.tf
@@ -49,19 +49,19 @@ resource "aws_security_group" "MyLab_sec_Group" {
   vpc_id = aws_vpc.MyLab-Vpc.id
 
   ingress {
-      from_port = 22
-      to_port = 22
-      protocol = "tcp"
-      cidr_blocks = [ "0.0.0.0/0" ]
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = [ "0.0.0.0/0" ]
 
   }
   egress = {
-     from_port = 0
-     to_port = 0
-     protocol = "1"
-     cidr_block = ["0.0.0.0/0"]
+   from_port = 0
+   to_port = 0
+   protocol = "1"
+   cidr_block = ["0.0.0.0/0"]
 
-   tags = {
+tags = {
     Name = " Allow traffic"
    } 
    }
