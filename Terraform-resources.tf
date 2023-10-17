edf9bbfc55c9c201a22b5aa468b237ea0d58eb2a
maki-haji <moafarah99@gmail.com>
2023-10-17 22:13:09 +0100

    bucket-update

--- Terraform-resources.tf
+++ Terraform-resources.tf
@@ -69,8 +69,8 @@ resource "aws_s3_bucket" "example" {
   bucket = "my-tf-test-bucket"
 
   tags = {
-    Name        = "My bucket"
-    Environment = "Dev"
+    Name        = "My-Best-bucket"
+    
   }
 }
 
